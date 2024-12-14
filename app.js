4e15031223edfe3a0974b0fea934023fdfa194d0
zadams1111 <73297253+zadams1111@users.noreply.github.com>
2024-12-13 21:06:35 -0500

    setup database connection

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ import router from './router/router.js'
 
 const app = express();
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+
+
 app.use("", router);
 
+app.listen(3401);
 
--- router/router.js
+++ router/router.js
@@ -4,6 +4,13 @@ import express from 'express'
 const router = express.Router()
 
 
+router.get('/', (req, res)=>{
+
+    res.render("index.ejs");
+
+})
+
+
 
 
 export default router;
